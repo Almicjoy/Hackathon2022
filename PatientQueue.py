ef7e60f59990409d7c0b1c7bebfef3eb27e55fce
Alessa Castillo <amcastillo@valdosta.edu>
2022-10-22 13:38:55 -0400

    queue

--- PatientQueue.py
+++ PatientQueue.py
@@ -1,5 +1,4 @@
 import Patient
-import main
 import sqlite3
 
 patient_list = []
@@ -7,13 +6,8 @@ patient_list = []
 def add_patient(patient):
     patient_list.append(patient)
 
-# def removePatient():
-#     p = patient_list[0]
-#     patient_list.pop(0)
-#     return p
-
-
-
+def removePatient():
+    patient_list.pop(0)
 
 def display_queue():
     for patient in patient_list:
@@ -23,12 +17,11 @@ def create_patient():
     connect = sqlite3.connect("instance/hospital.db")
     cursor = connect.cursor()
     for x in cursor.execute("SELECT * FROM patients_db"):
-        str = x[1].split(" ")
-        p = Patient.Patient(str[0], str[1], x[0], 0)
-        add_patient(p)
-
-# p = removePatient()
-# pri
+        if x[0] == 0:
+            str = x[1].split(" ")
+            p = Patient.Patient(str[0], str[1], x[0], 0)
+            add_patient(p)
+        
 
 create_patient()
 display_queue()
